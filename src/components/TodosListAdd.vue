f220633e9b007bd5324c0061ecb3f4587fffc620
Francois <francoistambwe@gmail.com>
2022-02-08 14:29:11 +0100

    stuff

---
+++ src/components/TodosListAdd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodosListAdd"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodosListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul v-if="data && data.length > 0">
+    <li v-for="item in data" key="item.id">
+       <span @click="$emit('on-todo-click', item)">{{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TodosListItem",
+  components: {},
+
+  props: ['data']
+}
+</script>
+
+
+
+
+
+
+<style scoped>
+
+</style>
