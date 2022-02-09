5cdb7e44cddeb0f1306682660a046e875b04c3d9
Francois <francoistambwe@gmail.com>
2022-02-09 11:03:48 +0100

    stuff

--- src/components/TodosListItem.vue
+++ src/components/TodosListItem.vue
@@ -1,8 +1,10 @@
 <template>
     <ul v-if="data && data.length > 0">
         <li v-for="item in data" :key="item.id">
-
             <span @click="$emit('on-todo-click', item)">{{ item.text }}</span>
+			<b-button @click="$emit('on-click-delete',item )" class="btn-sm btn-danger float-right del">
+				<font-awesome-icon icon="trash-alt"/>
+			</b-button>
 
         </li>
     </ul>
@@ -18,17 +20,24 @@ export default {
 
 <style scoped>
 ul {
-    width: 50%;
+    width: 100%;
     margin: auto;
     list-style: none;
     max-height: 200px;
     overflow-y: auto;
+
 }
 ul li {
     text-align: left;
     cursor: pointer;
+	width: 100%;
+	border: 1px solid #000;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 }
 ul li span {
     margin-left: 5px;
 }
+
 </style>
