3deb6ddf3c5d850616e7482ca7018c6b834e9bf6
bakrihamza <bakrihamza2209@gmail.com>
2022-02-09 11:02:49 +0100

    stuff

--- src/components/TodosListItem.vue
+++ src/components/TodosListItem.vue
@@ -2,6 +2,10 @@
     <ul v-if="data && data.length > 0">
         <li v-for="item in data" :key="item.id">
             <span @click="$emit('on-todo-click', item)">{{ item.text }}</span>
+
+			<b-button  @click="$emit('on-click-delete',item)" class="btn-sm btn-danger float-right del">
+				<font-awesome-icon icon="trash-alt"/>
+			</b-button>
         </li>
     </ul>
     <h3 v-else>Bitte warten</h3>
@@ -21,9 +25,16 @@ ul {
     list-style: none;
     max-height: 200px;
     overflow-y: auto;
+
 }
 ul li {
     text-align: left;
     cursor: pointer;
+	width: 100%;
+	border: 1px solid #000;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 }
+
 </style>
