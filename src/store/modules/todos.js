e0c092cd7907a324a0edf422832a9a904d6b7da6
bakrihamza <bakrihamza2209@gmail.com>
2022-02-10 13:08:58 +0100

    stuff

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -7,14 +7,32 @@ Vue.use(Vuex)
 const todos = {
 	namespaced: true,
 	state: {
-		todos: [],
+		liste: [],
+		todo:null,
+		error: null
 	},
 	getters: {
+		liste: state=>state.liste,
+		todo: state=>state.todo,
+		error: state=>state.error
 	},
 	mutations: {
+		mTodos:function (state, todos) {
+			state.liste = todos
+			state.error = null
+		},
 	},
 	actions: {
+		getTodos(context) {
+			axios.get("/api/todos")
+				.then(resp => {
+					context.commit('mTodos', resp.data)
+				})
+				.catch(err => {
+					context.commit('mError', err.response.data.message)
+				})
 
+		},
 	},
 };
 export default todos;
