67d66a4e87cf891c12438276f3e1fca3842fdf5d
berndengels <engels@f50.de>
2022-02-13 16:24:42 +0100

    stuff

--- src/store/modules/locale.js
+++ src/store/modules/locale.js
@@ -3,29 +3,30 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+const localeHeaderName = process.env.VUE_APP_LOCALE_HEADER_NAME,
+	locale = {
+		namespaced: true,
+		state: {
+			lang: 'de',
+		},
+		getters: {
+			lang: state => state.lang,
+		},
+		mutations: {
+			mSetLang: function (state, lang) {
+				state.lang = lang;
+				axios.defaults.headers.common[localeHeaderName] = lang;
+			},
+			mRemoveLang: function (state) {
+				state.lang = null;
+				delete axios.defaults.headers.common[localeHeaderName];
+			},
+		},
+		actions: {
+			setLang({commit}, language) {
+				commit('mSetLang', language)
+			}
+		},
+	};
 
-const locale = {
-	namespaced: true,
-	state: {
-		lang: 'de',
-	},
-	getters: {
-		lang: state => state.lang,
-	},
-	mutations: {
-		mSetLang: function (state, lang) {
-			state.lang = lang;
-			axios.defaults.headers.common['X-Lang'] = lang;
-		},
-		mRemoveLang: function (state) {
-			state.lang = null;
-			delete axios.defaults.headers.common['X-Lang'];
-		},
-	},
-	actions: {
-		setLang({commit}, language) {
-			commit('mSetLang', language)
-		}
-	},
-}
 export default locale;
